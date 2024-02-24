57a4d88a6b3d99886a4e9c82cbaa9022c06ca4de
Jaraf Salma <115297113+salmajaraf@users.noreply.github.com>
2024-02-24 12:47:11 +0100

    last changes

--- app/src/main/java/com/example/animauxsauvages/DetailAnimal.java
+++ app/src/main/java/com/example/animauxsauvages/DetailAnimal.java
@@ -3,48 +3,60 @@ package com.example.animauxsauvages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 public class DetailAnimal extends AppCompatActivity {
-    VideoView video;
-    Button b1,b2,b3;
+
+    Button b3,btnStart,btnPause;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_animal);
-
-        b1=findViewById(R.id.b1);
-        b2=findViewById(R.id.b2);
         b3=findViewById(R.id.b3);
+        btnStart = findViewById(R.id.b1);
+        btnPause = findViewById(R.id.b2);
 
-        b1.setOnClickListener(new View.OnClickListener() {
+
+        Bundle extras = getIntent().getExtras();
+        String videoUrl = extras.getString("videoUrl");
+        String description = extras.getString("description");
+
+        VideoView videoView = findViewById(R.id.v1);
+        videoView.setVideoURI(Uri.parse(videoUrl));
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+        TextView t1 = findViewById(R.id.t1);
+        t1.setText(description);
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                video.start();
+                videoView.start();
             }
         });
-        b2.setOnClickListener(new View.OnClickListener() {
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                video.pause();
+                videoView.pause();
             }
         });
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+                Intent i1= new Intent(DetailAnimal.this, MainActivity.class);
+                startActivity(i1);
             }
         });
-
-
-
-        video=findViewById(R.id.v1);
-        String path = "android.resource://" + getPackageName() + "/" + R.raw.lion;
-        video.setVideoPath(path);
 
 
     }
--- app/src/main/java/com/example/animauxsauvages/MainActivity.java
+++ app/src/main/java/com/example/animauxsauvages/MainActivity.java
@@ -2,30 +2,66 @@ package com.example.animauxsauvages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ImageView img1,img2,img3,img4,img5,img6;
 
-    Button b100;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        b100=findViewById(R.id.b100);
-
-        b100.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i2 = new Intent(getApplicationContext(), DetailAnimal.class);
-                startActivity(i2);
-            }
-        });
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        img3 = findViewById(R.id.img3);
+        img4 = findViewById(R.id.img4);
+        img5 = findViewById(R.id.img5);
+        img6 = findViewById(R.id.img6);
+        img1.setOnClickListener(this);
+        img2.setOnClickListener(this);
+        img3.setOnClickListener(this);
+        img4.setOnClickListener(this);
+        img5.setOnClickListener(this);
+        img6.setOnClickListener(this);
     }
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, DetailAnimal.class);
+        String videoUrl = "";
+        String description = "";
+
+        if (v.getId() == R.id.img1) {
+            videoUrl =  "android.resource://" + getPackageName() + "/raw/elephant";
+            description = "Les éléphants sont les plus grands mammifères terrestres du monde, avec leurs défenses distinctives et leurs grandes oreilles. Ils se trouvent dans divers habitats, des forêts tropicales aux savanes et aux déserts.";
+        } else if (v.getId() == R.id.img2) {
+            videoUrl =  "android.resource://" + getPackageName() + "/raw/girafe";
+            description = "Avec leur long cou élancé et leurs taches distinctives, les girafes sont les plus grands mammifères terrestres du monde. Elles se trouvent principalement dans les savanes et les zones arborées d'Afrique, se nourrissant principalement des feuilles des arbres acacias.";
+        } else if (v.getId() == R.id.img3) {
+            videoUrl =  "android.resource://" + getPackageName() + "/raw/lion";
+            description = "Les lions sont de puissants félins majestueux, connus pour leur crinière distinctive et leur statut de roi des animaux. Ils habitent les vastes plaines et savanes d'Afrique, vivant en groupes sociaux appelés fiertés.";
+        } else if (v.getId() == R.id.img4) {
+            videoUrl = "android.resource://" + getPackageName() + "/raw/singe";
+            description = "Les singes sont des créatures agiles et intelligentes appartenant à l'ordre des primates. Arborant souvent une queue et un visage expressif, ils se trouvent dans une variété d'habitats, des forêts tropicales aux montagnes.";
+        } else if (v.getId() == R.id.img5) {
+            videoUrl = "android.resource://" + getPackageName() + "/raw/gorille";
+            description = "Les gorilles, les plus grands primates vivants, se distinguent par leur corps massif et leur tête large. Vivant dans les forêts tropicales d'Afrique, ils sont principalement herbivores, se nourrissant de feuilles et de fruits.";
+        }else if (v.getId() == R.id.img6) {
+            videoUrl = "android.resource://" + getPackageName() + "/raw/crocodile";
+            description = "Les crocodiles sont des prédateurs redoutables des eaux douces, avec leur corps allongé, leur queue musclée et leurs mâchoires garnies de dents coniques. Ils résident principalement dans les régions tropicales et subtropicales, où ils chassent dans les rivières, les marécages et les lacs.";
+        }
+        intent.putExtra("videoUrl", videoUrl);
+        intent.putExtra("description", description);
+
+        startActivity(intent);
+    }
+
 }
